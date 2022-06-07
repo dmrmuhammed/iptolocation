8d5d5a90765964e59de9dbbfe9825b61534022c6
Muhammed Demir <mbayramdemir@gmail.com>
2022-06-07 20:05:33 +0300

    Prisma schema update & db seed

--- node/prisma/seed.js
+++ node/prisma/seed.js
@@ -7,22 +7,51 @@ const csv = require("fast-csv");
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log("Start seeding...");
-  fs.createReadStream(path.resolve(__dirname, "../../exampleData.CSV"))
+  let csvData = [];
+  let stream = fs
+    .createReadStream(path.resolve(__dirname, "../../exampleData2.CSV"))
     .pipe(csv.parse({ headers: false }))
     .on("error", (error) => console.error(error))
     .on("data", async function (data) {
-      const number = await prisma.number.create({
-        data: {
-          from: data[0],
-          to: data[1],
-          code: data[2],
-          name: data[3],
-        }
+      csvData.push({
+        from: data[0],
+        to: data[1],
+        countryCode: data[2],
+        countryName: data[3],
+        region: data[4],
+        city: data[5],
+        latitude: parseFloat(data[6]),
+        longitude: parseFloat(data[7]),
+        zipCode: data[8],
+        timeZone: data[9],
+        ispName: data[10],
+        domainName: data[11],
+        netSpeed: data[12],
+        iddCode: data[13],
+        areaCode: data[14],
+        weatherStationCode: data[15],
+        weatherStationName: data[16],
+        mcc: data[17],
+        mnc: data[18],
+        mobileBrand: data[19],
+        elevation: parseFloat(data[20]),
+        usageType: data[21],
       });
-      console.log(`Created number with id: ${number.id}`);
     })
-    .on("end", (rowCount) => console.log(`Parsed ${rowCount} rows`));
+    .on("end", async () => {
+      console.log("CSV parsing finished");
+      const t = 10000;
+      for (let i = 0; i < csvData.length / t; i++) {
+        await prisma.number
+          .createMany({
+            data: csvData.slice(i * t, t * (i + 1)),
+            skipDuplicates: true,
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
 }
 
 main()
--- node/server.js
+++ node/server.js
@@ -19,10 +19,12 @@ app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:8080" }));
 
 // Routes
-app.get("/", async (req, res) => {
-  const numbers = await prisma.number.findMany();
-  res.json(numbers);
-});
+// app.get("/", async (req, res) => {
+//   const numbers = await prisma.number.findMany({
+//     take: 50,
+//   });
+//   res.json(numbers);
+// });
 
 app.post("/", async (req, res) => {
   const { ip } = req.body;
@@ -33,10 +35,11 @@ app.post("/", async (req, res) => {
   }
 
   const arr = parser.parseIp(ip);
-  const str =
-    arr[0] * 256 * 256 * 256 + arr[1] * 256 * 256 + arr[2] * 256 + arr[3];
-  const number = parseFloat(str);
-  const exampleIpNumber = 2957694646;
+  const number =
+    parseFloat(arr[0]) * 256 * 256 * 256 +
+    parseFloat(arr[1]) * 256 * 256 +
+    parseFloat(arr[2]) * 256 +
+    parseFloat(arr[3]);
 
   const numbers = await prisma.number.findMany({
     where: {
@@ -49,19 +52,13 @@ app.post("/", async (req, res) => {
             gt: number,
           },
         },
-        {
-          from: {
-            lt: exampleIpNumber,
-          },
-          to: {
-            gt: exampleIpNumber,
-          },
-        },
       ],
     },
   });
 
-  res.json(numbers);
+  if (numbers.length === 0) {
+    res.json({ error: "No number found on database!" });
+  } else res.json(numbers);
 });
 
 // Start Server
